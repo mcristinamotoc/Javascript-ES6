8e7f01220e5ef670951dbbca12a793bc221ebb1d
Cristina M <mcristinamotoc@gmail.com>
2021-01-06 14:29:28 +0100

    Ex.2

---
+++ Ex.2.js
@@ -0,0 +1,26 @@
+/*Exercici 2
+Arregla l 'error del següent bloc de codi:
+
+users.map(function(user) {
+    user.firstName;
+});
+
+var users = [{ firstName: 'Homer', lastName: 'Simpson' },
+    { firstName: 'Marge', lastName: 'Simpson' },
+    { firstName: 'Bart', lastName: 'Simpson' },
+    { firstName: 'Lisa', lastName: 'Simpson' },
+    { firstName: 'Maggie', lastName: 'Simpson' }
+];*/
+
+let users = [{ firstName: 'Homer', lastName: 'Simpson' },
+    { firstName: 'Marge', lastName: 'Simpson' },
+    { firstName: 'Bart', lastName: 'Simpson' },
+    { firstName: 'Lisa', lastName: 'Simpson' },
+    { firstName: 'Maggie', lastName: 'Simpson' }
+];
+
+let usersFirstName = users.map((user) => {
+    return user.firstName;
+});
+
+console.log(usersFirstName);
